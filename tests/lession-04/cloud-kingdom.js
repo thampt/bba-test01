3fa5bf3a557001f3dc0bb8c53d678f0127a3a012
test01 - Tham Pham <thampt.tester@gmail.com>
2026-04-14 23:50:30 +0900

    feat: add solution for challenge 03

---
+++ tests/lession-04/cloud-kingdom.js
@@ -0,0 +1,6 @@
+let playerName = "Mario", currentLives = 3;
+const level1 = 25, level2 = 30, level3 = 45;
+let total = level1 + level2 + level3;
+let avg = total / 3;
+console.log("tong coin: " + total + "\ntrung binh coin: " + avg);
+console.log("so du % 3: ", total%3);
